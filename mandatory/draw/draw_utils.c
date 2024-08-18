f6c59d6875ae1cab422bb332545bef0d39eaef36
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-08-18 11:36:53 +0100

    by gacp

---
+++ mandatory/draw/draw_utils.c
@@ -0,0 +1,146 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/16 16:22:02 by youmoukh          #+#    #+#             */
+/*   Updated: 2024/08/17 19:49:43 by youmoukh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub.h"
+
+int 	check_view(t_cube *game, t_ray *ray)
+{
+	(void)ray;
+	if (game->map[(int)(ray->wall_y / BOX_SIZE)][(int)(ray->wall_x
+			/ BOX_SIZE)] == 'D')
+		game->img_wall = game->png.door;
+	else
+	{
+		if (!game->hit_v)
+		{
+			if (game->is_facingup)
+				game->img_wall = game->png.so;
+			else if (game->is_facingdown)
+				game->img_wall = game->png.no;
+		}
+		else
+		{
+			if (game->is_facingleft)
+				game->img_wall = game->png.ea;
+			if (game->is_facingright)
+				game->img_wall = game->png.we;
+		}
+	}
+	return game->img_wall->width / BOX_SIZE;
+}
+
+void	draw_wall_one(t_cube *game, t_ray *ray)
+{
+	game->draws.wall_heigth = (BOX_SIZE / (ray->distance
+				* cos(game->rotation_angle - game->ray_angle))) * ((WINDOW_WIDTH
+				/ 2) / tan(PI / 6));
+	game->draws.top = ft_max((1000 / 2) - (game->draws.wall_heigth / 2), 0);
+	game->draws.bottom = ft_min((1000 / 2) + (game->draws.wall_heigth / 2),
+			1000);
+	get_x(game, ray);
+	game->draws.i = game->draws.top;
+	game->draws.butt = game->draws.bottom;
+	game->draws.topp = game->draws.top;
+	game->draws.incr = 0;
+	while (game->draws.incr++ < game->draws.topp)
+		mlx_put_pixel(game->img, ray->index, game->draws.incr,
+			ft_color(game->colors.r_c, game->colors.g_c,
+				game->colors.b_c, 255));
+	game->draws.incr = WINDOW_HEITH;
+	while (game->draws.incr > game->draws.butt)
+	{
+		if (game->draws.butt < 0)
+			game->draws.butt = 0;
+		mlx_put_pixel(game->img, ray->index, game->draws.butt,
+			ft_color(game->colors.r_f, game->colors.g_f, game->colors.b_f,
+				255));
+		game->draws.butt++;
+	}
+}
+void ft_handle_image(t_cube *game , t_ray *ray , int x)
+{
+	while(x > 0){
+		game->draws.dis = game->draws.i - WINDOW_HEITH / 2
+			+ game->draws.wall_heigth / 2;
+		game->offset_y = game->draws.dis * ((float)BOX_SIZE
+				/ game->draws.wall_heigth);
+		game->draws.j = ((game->offset_x + game->offset_y * BOX_SIZE) * 4);
+		game->draws.r = game->img_wall->pixels[game->draws.j];
+		game->draws.g = game->img_wall->pixels[game->draws.j + 1];
+		game->draws.b = game->img_wall->pixels[game->draws.j + 2];
+		game->draws.a = game->img_wall->pixels[game->draws.j + 3];
+		mlx_put_pixel(game->img, ray->index, game->draws.i,
+			ft_color(game->draws.r, game->draws.g, game->draws.b,
+			game->draws.a));
+		x--;
+	}
+}
+void	ft_draw_wall(t_cube *game, t_ray *ray)
+{
+	draw_wall_one(game, ray);
+	game->draws.i = game->draws.top;
+	int z;
+	int x = 0;
+	while (game->draws.i < game->draws.bottom)
+	{
+		z = check_view(game, ray);
+		if (z == 0)
+			x = BOX_SIZE / game->img_wall->width ;
+		if (!x){
+			game->draws.dis = game->draws.i - WINDOW_HEITH / 2
+				+ game->draws.wall_heigth / 2;
+			game->offset_y = game->draws.dis * ((float)BOX_SIZE
+					/ game->draws.wall_heigth);
+			game->draws.j = ((game->offset_x + game->offset_y * BOX_SIZE) * 4);
+			game->draws.r = game->img_wall->pixels[game->draws.j];
+			game->draws.g = game->img_wall->pixels[game->draws.j + 1];
+			game->draws.b = game->img_wall->pixels[game->draws.j + 2];
+			game->draws.a = game->img_wall->pixels[game->draws.j + 3];
+			mlx_put_pixel(game->img, ray->index, game->draws.i,
+				ft_color(game->draws.r, game->draws.g, game->draws.b,
+					game->draws.a));
+			game->draws.i+=z;
+		}
+		else
+		{
+			ft_handle_image(game , ray , x);
+			game->draws.i++;
+		}
+	}
+}
+
+void	draw_line_dda(t_cube *game)
+{
+	int	i;
+	int	colun;
+
+	colun = 0;
+	i = 0;
+	game->ray_angle = (game->rotation_angle - (game->field_of_view_angle / 2));
+	while (i < game->num_ray)
+	{
+		ray_cast(colun, game);
+		game->ray_angle += (game->field_of_view_angle / game->num_ray);
+		i++;
+		colun++;
+	}
+}
+
+int	check_me(int x, int y, int width, int heigth)
+{
+	if (x / BOX_MINI >= 0 && x
+		/ BOX_MINI <= width / BOX_SIZE && y
+		/ BOX_MINI >= 0 && y
+		/ BOX_MINI <= heigth / BOX_SIZE)
+		return (0x1);
+	return (0x0);
+}
---
+++ mandatory/parsing/parse.c
@@ -0,0 +1,132 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/01 13:27:24 by youmoukh          #+#    #+#             */
+/*   Updated: 2024/08/17 17:31:31 by youmoukh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub.h"
+
+char	*fix_the_map(char *s, int i, int flag, int j)
+{
+	char	*str;
+	char	*tmp;
+
+	tmp = ft_strtrim(s, " ");
+	while (tmp[i++])
+	{
+		if (tmp[i] == 32 || tmp[i] == 9)
+			flag = 0x1;
+	}
+	if (flag == 0x1)
+	{
+		i = 0x0;
+		str = ft_calloc(0x1, ft_strlen(s) + 0x1);
+		if (!str)
+			return (NULL);
+		while (tmp[i++])
+		{
+			if (!(tmp[i] == 32 || tmp[i] == 9))
+				str[j++] = tmp[i];
+		}
+	}
+	if (flag)
+		return (str);
+	return (tmp);
+}
+
+int	get_length_heigth(char **str, int mode, int i)
+{
+	if (!mode)
+	{
+		while (str[i])
+			i++;
+		i -= 6;
+	}
+	if (mode)
+	{
+		while (str[mode][i])
+			i++;
+	}
+	return (i);
+}
+
+char	**final_map(t_cube *game, char **str)
+{
+	int		i;
+	int		j;
+	char	**s;
+
+	i = 0x0;
+	s = (char **)ft_malloc((game->real_map_heigth + 0x1) * 8, 1);
+	if (!s)
+		return (NULL);
+	j = 0x0;
+	while (str[i])
+	{
+		if (i > 0x5)
+		{
+			s[j] = fill_chars(game, str[i]);
+			if (!s[j++])
+				return (NULL);
+		}
+		i++;
+	}
+	s[j] = 0;
+	return (s);
+}
+
+void	check_position(t_cube *game, char **s, int i, int j)
+{
+	while (s[i])
+	{
+		j = 0;
+		while (s[i][j])
+		{
+			if (peter_csezsh(s, i, j))
+			{
+				if (game->player_vision == 1)
+					if (case_1(s, i, j))
+						error_message(game, 11);
+				if (game->player_vision == 2)
+					if (case_2(s, i, j))
+						error_message(game, 11);
+				if (game->player_vision == 3)
+					if (case_3(s, i, j))
+						error_message(game, 11);
+				if (game->player_vision == 4)
+					if (case_4(s, i, j))
+						error_message(game, 11);
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+void	parse(int ac, char *file, t_cube *game)
+{
+	if (ac <= 1 || ac >= 3)
+		error_message(game, 0x1);
+	if (check_extension(file, 0x0) == 0)
+		error_message(game, 0x2);
+	game->map_2d = read_map_from_file(file, 0x0, 0x0);
+	if (!game->map_2d)
+		error_message(game, 0x3);
+	init_counter(game);
+	heigth_width(game);
+	check_texture_intra(game, 0x0);
+	check_valid_members(game, 0x0, 0x0);
+	player_vision(game->map_2d, game);
+	parse_entry(game, 0x0);
+	ft_load_textures(game);
+	game->map = final_map(game, game->map_2d);
+	check_player(game, game->map);
+	check_textures(game);
+	check_position(game, game->map, 0, 0);
+}
