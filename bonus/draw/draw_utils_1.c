6b39e91a4bfdc5a15fb77967f3e63662b582c227
ABDOLMOUGHIT ECHCHARBINY <111705320+mrmoughit@users.noreply.github.com>
2024-08-19 15:19:59 +0100

    abdelmoughit

---
+++ bonus/draw/draw_utils_1.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw_utils_1.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abechcha <abechcha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/17 19:46:20 by youmoukh          #+#    #+#             */
+/*   Updated: 2024/08/18 17:55:24 by abechcha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub_bonus.h"
+
+int	check_view(t_cube *game, t_ray *ray)
+{
+	(void)ray;
+	if (game->map[(int)(ray->wall_y / BOX_SIZE)][(int)(ray->wall_x
+			/ BOX_SIZE)] == 'D')
+		game->img_wall = game->png.door;
+	else
+	{
+		if (!game->hit_v)
+		{
+			if (game->is_facingup)
+				game->img_wall = game->png.so;
+			else if (game->is_facingdown)
+				game->img_wall = game->png.no;
+		}
+		else
+		{
+			if (game->is_facingleft)
+				game->img_wall = game->png.ea;
+			if (game->is_facingright)
+				game->img_wall = game->png.we;
+		}
+	}
+	return (game->img_wall->width / BOX_SIZE);
+}
+
+void	draw_wall_one(t_cube *game, t_ray *ray)
+{
+	game->draws.wall_heigth = (BOX_SIZE / (ray->distance
+				* cos(game->rotation_angle - game->ray_angle))) * ((WINDOW_WIDTH
+				/ 2) / tan(PI / 6));
+	game->draws.top = ft_max((WINDOW_HEIGTH / 2) - (game->draws.wall_heigth / 2), 0);
+	game->draws.bottom = ft_min((WINDOW_HEIGTH / 2) + (game->draws.wall_heigth / 2),
+			WINDOW_HEIGTH);
+	get_x(game, ray);
+	game->draws.i = game->draws.top;
+	game->draws.butt = game->draws.bottom;
+	game->draws.topp = game->draws.top;
+	game->draws.incr = 0;
+	while (game->draws.incr++ < game->draws.topp)
+		mlx_put_pixel(game->img, ray->index, game->draws.incr,
+			ft_color(game->colors.r_c, game->colors.g_c, game->colors.b_c,
+				255));
+	game->draws.incr = WINDOW_HEIGTH;
+	while (game->draws.incr > game->draws.butt)
+	{
+		if (game->draws.butt < 0)
+			game->draws.butt = 0;
+		mlx_put_pixel(game->img, ray->index, game->draws.butt,
+			ft_color(game->colors.r_f, game->colors.g_f, game->colors.b_f,
+				255));
+		game->draws.butt++;
+	}
+}
+
+void	ft_handle_image(t_cube *game, t_ray *ray, int x)
+{
+	while (x > 0)
+	{
+		game->draws.dis = game->draws.i - WINDOW_HEIGTH / 2
+			+ game->draws.wall_heigth / 2;
+		game->offset_y = game->draws.dis * ((float)BOX_SIZE
+				/ game->draws.wall_heigth);
+		game->draws.j = ((game->offset_x + game->offset_y * BOX_SIZE) * 4);
+		game->draws.r = game->img_wall->pixels[game->draws.j];
+		game->draws.g = game->img_wall->pixels[game->draws.j + 1];
+		game->draws.b = game->img_wall->pixels[game->draws.j + 2];
+		game->draws.a = game->img_wall->pixels[game->draws.j + 3];
+		mlx_put_pixel(game->img, ray->index, game->draws.i,
+			ft_color(game->draws.r, game->draws.g, game->draws.b,
+				game->draws.a));
+		x--;
+	}
+}
---
+++ bonus/parsing/parse_tools.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_tools.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/04 11:06:27 by youmoukh          #+#    #+#             */
+/*   Updated: 2024/08/17 14:22:12 by youmoukh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub_bonus.h"
+
+void	check_texture_intra(t_cube *game, int i)
+{
+	char	*str;
+
+	while (game->map_2d[i] && i < 0x6)
+	{
+		str = ft_substr(game->map_2d[i], 0x0, 2);
+		if (!ft_strcmp(str, "NO"))
+			game->cnt.a1++;
+		else if (!ft_strcmp(str, "SO"))
+			game->cnt.a2++;
+		else if (!ft_strcmp(str, "EA"))
+			game->cnt.a3++;
+		else if (!ft_strcmp(str, "C "))
+			game->cnt.a4++;
+		else if (!ft_strcmp(str, "F "))
+			game->cnt.a5++;
+		else if (!ft_strcmp(str, "WE"))
+			game->cnt.a6++;
+		i++;
+	}
+	if (ultra_check(game, 0))
+		error_message(game, 0x4);
+}
+
+void	init_counter(t_cube *game)
+{
+	game->cnt.a1 = 0;
+	game->cnt.a2 = 0;
+	game->cnt.a3 = 0;
+	game->cnt.a4 = 0;
+	game->cnt.a5 = 0;
+	game->cnt.a6 = 0;
+	game->parse_p.n = 0;
+	game->parse_p.s = 0;
+	game->parse_p.w = 0;
+	game->parse_p.e = 0;
+}
+
+int	ft_strlen_ii(char *s)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = 0;
+	while (s[i])
+	{
+		if (s[i] != 9)
+			len++;
+		i++;
+	}
+	return (len);
+}
+
+void	heigth_width(t_cube *game)
+{
+	int	i;
+	int	j;
+
+	i = 6;
+	j = 0;
+	while (game->map_2d[i])
+	{
+		if (j <= ft_strlen_ii(game->map_2d[i]))
+			j = ft_strlen_ii(game->map_2d[i]);
+		i++;
+	}
+	game->real_map_width = j;
+	game->real_map_heigth = get_length_heigth(game->map_2d, 0, 6);
+}
+
+int	player_num(t_cube *game)
+{
+	if ((game->parse_p.n + game->parse_p.s
+			+ game->parse_p.w + game->parse_p.e) == 0x0)
+		return (0x1);
+	if ((game->parse_p.n + game->parse_p.e
+			+ game->parse_p.w + game->parse_p.s) > 0x1)
+		return (0x1);
+	if (game->parse_p.n > 0x1
+		|| game->parse_p.e > 0x1
+		|| game->parse_p.w > 0x1
+		|| game->parse_p.s > 0x1)
+		return (0x1);
+	if (game->parse_p.n != 0)
+		game->player_vision = 1;
+	else if (game->parse_p.s != 0)
+		game->player_vision = 2;
+	else if (game->parse_p.w != 0)
+		game->player_vision = 3;
+	else if (game->parse_p.e != 0)
+		game->player_vision = 4;
+	return (0x0);
+}
